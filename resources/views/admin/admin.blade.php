e8c7dcf0287059f84ae94e39ba5758acde03e219
unknown <galaxyasamsung979@gmail.com>
2022-07-30 19:10:56 +0500

    admin panel orgerdi

--- resources/views/admin/admin.blade.php
+++ resources/views/admin/admin.blade.php
@@ -11,29 +11,28 @@
         <thead>
             <tr>
                 <td>Nomeri</td>
-                <td> File </td>
+                <td>File</td>
                 <td>Massege</td>
-                <td> Delete </td>
                 <td>Status</td>
+                <td>Delete</td>
             </tr>
         </thead>
         <tbody>
         @forelse($forms as $form)
         <tr>
             <td>{{ $loop->index+1 }}</td>
-            <td><img src="{{ $form->file }}" alt=""  width="50"></td>
+            <td><img src="{{ asset('storage/'.$form->file) }}" alt="foto" width="50"></td>
             <td>{{ $form->massege }}</td>
             <td>Status</td>
-            <td><form action="{{ route('admin.destroy',$form->id)}}" method="POST">
+            <td><form action="{{ route('destroy',$form->id)}}" method="POST">
             @method('DELETE')    
             @csrf
             <input type="submit" value="delete">
             </form>
-            </td>
-            <td>Status</td>
+            </td>    
         </tr>
         @empty
-            <br>    magliwmatlar joq<br>
+            <br>magliwmatlar joq<br>
         @endforelse
         </tbody>
 
